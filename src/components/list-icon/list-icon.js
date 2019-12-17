4a8975368448e2a927da4359b76dc9da7ea0beeb
m-moiz <muhammadmoiz856@hotmail.com>
2019-12-17 16:56:34 -0500

    Added iconContext to add props to icons

--- src/components/list-icon/list-icon.js
+++ src/components/list-icon/list-icon.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { IconContext } from 'react-icons';
 
 const Wrapper = styled.div`
 	margin-right: .5rem;
@@ -8,6 +9,10 @@ const Wrapper = styled.div`
 	}
 `;
 
-const ListIcon = ({ children, color }) => <Wrapper color={color}>{children}</Wrapper>;
+const ListIcon = ({ children, color }) => (
+	<IconContext.Provider value={{ color: color }}>
+		<Wrapper>{children}</Wrapper>
+	</IconContext.Provider>
+);
 
 export default ListIcon;
